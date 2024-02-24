6622693bcf5dbc72ac7e0f26ccc90dfeedd49a57
Martin M <mcm57@gmx.at>
2024-02-24 19:40:46 +0100

    Update package.js

--- test/package.js
+++ test/package.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { tests } = require('@iobroker/testing');
+const { tests } = require('@mcm4iob/testing');
 
 // Validate the package files
 tests.packageFiles(path.join(__dirname, '..'));
